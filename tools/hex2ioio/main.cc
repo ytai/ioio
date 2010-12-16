91a64298cff7b7b491dac5e2678d63a7c6321fd9
Ytai Ben-Tsvi <ytaibt@gmail.com>
2010-12-17 01:52:42 +0200

    hex2ioio working

--- tools/hex2ioio/main.cc
+++ tools/hex2ioio/main.cc
@@ -1,13 +1,43 @@
+// hex2ioio
+// This utility translate a hex file into a IOIO application firmware image.
+// Usage: hex2ioio <infile> <outfile>
+//
+// The input format is a standard hex, as produced by the C30 compiler. This
+// program does some sanity checking on it, but not full validation.
+//
+// The output format is very simple and intended to be:
+//   1. Light-weight
+//   2. Simple to parse by the IOIO
+// The file is a sequnence of blocks, each 196B long, containing:
+//   1. 4B address (little endian)
+//   2. 192B contents (equal to one Flash row in a PIC24), which are actually
+//      64 triplets of little-endian, 24-bit instructions.
+
 #include <iostream>
 #include <fstream>
 #include <map>
 using namespace std;
 
-// a block the size of one flash row = 64 instructions = 192 bytes
-typedef uint8_t block[192];
+// a block the size of one flash row = 64 instructions = 256 bytes
+// high (last) byte in each dword must be 0.
+class block_t {
+ public:
+  block_t() { memset(b_, 0xFF, sizeof b_); }
+
+  uint8_t& operator[] (size_t pos) { return b_[pos]; }
+
+  void serialize(ostream &out) const {
+    for (int i = 0; i < 256; ++i)
+      if ((i & 0x03) != 3)
+        out.put(b_[i]);
+  }
+
+ private:
+  uint8_t b_[256];
+};
 
 // a map from address (128-word aligned - 7LSB are 0) to block
-typedef map<uint32_t, block> memory_map_t;
+typedef map<uint32_t, block_t> memory_map_t;
 
 void usage() {
 cerr << "Usage: hex2ioio <in> <out>" << endl;
@@ -49,24 +79,54 @@ int main(int argc, const char* argv[]) {
 
   // process input hex file into memory map
   ifstream infile(argv[1], ios::in);
-  while (!infile.eof()) {
+
+  bool got_eof_rec = false;
+  uint16_t address_hi = 0;
+  while (!infile.eof() && !got_eof_rec) {
     ++line_num;
     char line[256];
     infile.getline(line, sizeof line);
     if (line[0] != ':') continue;
     const char * p = line + 1;
     uint8_t count = read8(p);
-	uint16_t address = read16(p);
+	uint16_t address_lo = read16(p);
 	uint8_t type = read8(p);
-	cerr << "Address 0x" << hex << address << dec << " type: " << int(type) << endl;
-    for (int i = 0; i < count; ++i) cerr << hex << int(read8(p)) << " ";
-	cerr << endl;
+	switch (type) {
+      case 0:  // data
+        {
+          uint32_t address = static_cast<uint32_t>(address_hi) << 16 | address_lo;
+          block_t &block = memory_map[address >> 8];
+		  for (int i = 0; i < count; ++i) {
+            uint8_t byte = read8(p);
+            block[address & 0xFF] = byte;
+		    if ((address & 0x03) == 3 && byte != 0) error("high byte of each dword must be 0");
+            ++address;
+          }
+		}
+		break;
+
+      case 1:  // eof
+        got_eof_rec = true;
+        break;
+
+      case 4:
+        address_hi = read16(p);
+        break;
+    }
 	read8(p);  // for checksum
     if (checksum) error("Failed checksum");
   }
+  infile.close();
+
+  cerr << "Read " << memory_map.size() << " blocks" << endl;
 
   // write memory map to output file
   ofstream outfile(argv[2], ios::out | ios::binary);
+  for (memory_map_t::const_iterator iter = memory_map.begin(); iter != memory_map.end(); ++iter) {
+    outfile.write(reinterpret_cast<const char*>(&iter->first), 4);
+    iter->second.serialize(outfile);
+  }
+  outfile.close();
 
   cerr << "Success!" << endl;
   return 0;
