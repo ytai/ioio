d1aaa788dfa16f971a04933dff1a790ecf71ffe0
Ytai Ben-Tsvi <ytaibt@gmail.com>
2010-12-17 13:59:26 +0200

    filegen now generates a legal ioio file

--- software/GenFile/src/ioio/filegen/MainActivity.java
+++ software/GenFile/src/ioio/filegen/MainActivity.java
@@ -3,6 +3,7 @@ package ioio.filegen;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 import android.app.Activity;
 import android.os.Bundle;
@@ -14,10 +15,13 @@ public class MainActivity extends Activity {
 		try {
 			super.onCreate(savedInstanceState);
 			setContentView(R.layout.main);
-			FileOutputStream fos = openFileOutput("test_file", MODE_WORLD_READABLE);
-			for (int i = 0; i < 20000; ++i) {
-				fos.write(i % 256);
+			FileOutputStream fos = openFileOutput("sample.ioio", MODE_WORLD_READABLE);
+			InputStream is = getAssets().open("sample.ioio");
+			int i;
+			while (-1 != (i = is.read())) {
+				fos.write(i);
 			}
+			is.close();
 			fos.close();
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
