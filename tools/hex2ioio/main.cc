9a70eeb64d855c10f736f606f01a0f951da5fc97
Ytai Ben-Tsvi <ytaibt@gmail.com>
2010-12-17 14:35:07 +0200

    fix small bug in hex2ioio

--- tools/hex2ioio/main.cc
+++ tools/hex2ioio/main.cc
@@ -101,8 +101,8 @@ int main(int argc, const char* argv[]) {
       case 0:  // data
         {
           uint32_t address = static_cast<uint32_t>(address_hi) << 16 | address_lo;
-          block_t &block = memory_map[address >> 8];
 		  for (int i = 0; i < count; ++i) {
+            block_t &block = memory_map[address & 0xFFFFFFF0];
             uint8_t byte = read8(p);
             block[address & 0xFF] = byte;
 		    if ((address & 0x03) == 3 && byte != 0) error("high byte of each dword must be 0");
