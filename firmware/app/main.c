dd948819a61770d88793612ae76f71e670259cf4
Ytai Ben-Tsvi <ytaibt@gmail.com>
2010-12-23 23:27:56 +0200

    made demo app blink faster

--- firmware/app/main.c
+++ firmware/app/main.c
@@ -3,10 +3,11 @@
 int main() {
   TRISFbits.TRISF3 = 0;
   while (1) {
-    long i = 0;
+    long i = 1000000L;
     LATFbits.LATF3 = 0;
-    while (++i);
+    while (i--);
+    i = 1000000L;
     LATFbits.LATF3 = 1;
-    while (++i);
+    while (i--);
   }
 }
