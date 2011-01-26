7613f1b4c3d9d710a57a240155425a6661e2c5e5
Shishir Birmiwal <ioio@shishir.org>
2011-01-25 23:08:57 -0800

    adding MPLAB X IDE project files for IOIO App

---
+++ firmware/app/IOIOApp.X/nbproject/Makefile-default.mk
@@ -0,0 +1,158 @@
+#
+# Generated Makefile - do not edit!
+#
+# Edit the Makefile in the project folder instead (../Makefile). Each target
+# has a -pre and a -post target defined where you can add customized code.
+#
+# This makefile implements configuration specific macros and targets.
+
+
+# Include project Makefile
+include Makefile
+
+# Environment
+MKDIR=mkdir -p
+RM=rm -f 
+CP=cp 
+# Macros
+CND_CONF=default
+
+ifeq ($(TYPE_IMAGE), DEBUG_RUN)
+IMAGE_TYPE=debug
+FINAL_IMAGE=dist/${CND_CONF}/${IMAGE_TYPE}/IOIOApp.X.${IMAGE_TYPE}.elf
+else
+IMAGE_TYPE=production
+FINAL_IMAGE=dist/${CND_CONF}/${IMAGE_TYPE}/IOIOApp.X.${IMAGE_TYPE}.elf
+endif
+# Object Directory
+OBJECTDIR=build/${CND_CONF}/${IMAGE_TYPE}
+# Distribution Directory
+DISTDIR=dist/${CND_CONF}/${IMAGE_TYPE}
+
+# Object Files
+OBJECTFILES=${OBJECTDIR}/_ext/1472/main.o ${OBJECTDIR}/_ext/1846862476/uart2.o
+
+
+CFLAGS=
+ASFLAGS=
+LDLIBSOPTIONS=
+
+OS_ORIGINAL="Darwin"
+OS_CURRENT="$(shell uname -s)"
+############# Tool locations ##########################################
+# If you copy a project from one host to another, the path where the  #
+# compiler is installed may be different.                             #
+# If you open this project with MPLAB X in the new host, this         #
+# makefile will be regenerated and the paths will be corrected.       #
+#######################################################################
+MP_CC=/Applications/microchip/mplabc30/v3.24/bin/pic30-gcc
+MP_AS=/Applications/microchip/mplabc30/v3.24/bin/pic30-as
+MP_LD=/Applications/microchip/mplabc30/v3.24/bin/pic30-ld
+MP_AR=/Applications/microchip/mplabc30/v3.24/bin/pic30-ar
+MP_CC_DIR=/Applications/microchip/mplabc30/v3.24/bin
+MP_AS_DIR=/Applications/microchip/mplabc30/v3.24/bin
+MP_LD_DIR=/Applications/microchip/mplabc30/v3.24/bin
+MP_AR_DIR=/Applications/microchip/mplabc30/v3.24/bin
+.build-conf: ${BUILD_SUBPROJECTS}
+ifneq ($(OS_CURRENT),$(OS_ORIGINAL))
+	@echo "***** WARNING: This make file contains OS dependent code. The OS this makefile is being run is different from the OS it was created in."
+endif
+	${MAKE}  -f nbproject/Makefile-default.mk dist/${CND_CONF}/${IMAGE_TYPE}/IOIOApp.X.${IMAGE_TYPE}.elf
+
+# ------------------------------------------------------------------------------------
+# Rules for buildStep: assemble
+ifeq ($(TYPE_IMAGE), DEBUG_RUN)
+else
+endif
+
+# ------------------------------------------------------------------------------------
+# Rules for buildStep: compile
+ifeq ($(TYPE_IMAGE), DEBUG_RUN)
+${OBJECTDIR}/_ext/1472/main.o: ../main.c  nbproject/Makefile-${CND_CONF}.mk
+	${MKDIR} ${OBJECTDIR}/_ext/1472 
+	${RM} ${OBJECTDIR}/_ext/1472/main.o.d 
+	${MP_CC} -g -D__DEBUG -D__MPLAB_DEBUGGER_PK3=1 -omf=elf -x c -c -mcpu=24FJ128DA106 -Wall -I".." -I"../.." -I"../../blapi" -I"../../bootloader/microchip/include" -MMD -MF ${OBJECTDIR}/_ext/1472/main.o.d -o ${OBJECTDIR}/_ext/1472/main.o ../main.c 
+ifneq (,$(findstring MINGW32,$(OS_CURRENT))) 
+	 sed -e 's/\"//g' -e 's/\\$$/__EOL__/g' -e 's/\\ /__ESCAPED_SPACES__/g' -e 's/\\/\//g' -e 's/__ESCAPED_SPACES__/\\ /g' -e 's/__EOL__$$/\\/g' ${OBJECTDIR}/_ext/1472/main.o.d > ${OBJECTDIR}/_ext/1472/main.o.tmp
+	${RM} ${OBJECTDIR}/_ext/1472/main.o.d 
+	${CP} ${OBJECTDIR}/_ext/1472/main.o.tmp ${OBJECTDIR}/_ext/1472/main.o.d 
+	${RM} ${OBJECTDIR}/_ext/1472/main.o.tmp}
+else 
+	 sed -e 's/\"//g' ${OBJECTDIR}/_ext/1472/main.o.d > ${OBJECTDIR}/_ext/1472/main.o.tmp
+	${RM} ${OBJECTDIR}/_ext/1472/main.o.d 
+	${CP} ${OBJECTDIR}/_ext/1472/main.o.tmp ${OBJECTDIR}/_ext/1472/main.o.d 
+	${RM} ${OBJECTDIR}/_ext/1472/main.o.tmp}
+endif
+${OBJECTDIR}/_ext/1846862476/uart2.o: ../../bootloader/microchip/common/uart2.c  nbproject/Makefile-${CND_CONF}.mk
+	${MKDIR} ${OBJECTDIR}/_ext/1846862476 
+	${RM} ${OBJECTDIR}/_ext/1846862476/uart2.o.d 
+	${MP_CC} -g -D__DEBUG -D__MPLAB_DEBUGGER_PK3=1 -omf=elf -x c -c -mcpu=24FJ128DA106 -Wall -I".." -I"../.." -I"../../blapi" -I"../../bootloader/microchip/include" -MMD -MF ${OBJECTDIR}/_ext/1846862476/uart2.o.d -o ${OBJECTDIR}/_ext/1846862476/uart2.o ../../bootloader/microchip/common/uart2.c 
+ifneq (,$(findstring MINGW32,$(OS_CURRENT))) 
+	 sed -e 's/\"//g' -e 's/\\$$/__EOL__/g' -e 's/\\ /__ESCAPED_SPACES__/g' -e 's/\\/\//g' -e 's/__ESCAPED_SPACES__/\\ /g' -e 's/__EOL__$$/\\/g' ${OBJECTDIR}/_ext/1846862476/uart2.o.d > ${OBJECTDIR}/_ext/1846862476/uart2.o.tmp
+	${RM} ${OBJECTDIR}/_ext/1846862476/uart2.o.d 
+	${CP} ${OBJECTDIR}/_ext/1846862476/uart2.o.tmp ${OBJECTDIR}/_ext/1846862476/uart2.o.d 
+	${RM} ${OBJECTDIR}/_ext/1846862476/uart2.o.tmp}
+else 
+	 sed -e 's/\"//g' ${OBJECTDIR}/_ext/1846862476/uart2.o.d > ${OBJECTDIR}/_ext/1846862476/uart2.o.tmp
+	${RM} ${OBJECTDIR}/_ext/1846862476/uart2.o.d 
+	${CP} ${OBJECTDIR}/_ext/1846862476/uart2.o.tmp ${OBJECTDIR}/_ext/1846862476/uart2.o.d 
+	${RM} ${OBJECTDIR}/_ext/1846862476/uart2.o.tmp}
+endif
+else
+${OBJECTDIR}/_ext/1472/main.o: ../main.c  nbproject/Makefile-${CND_CONF}.mk
+	${MKDIR} ${OBJECTDIR}/_ext/1472 
+	${RM} ${OBJECTDIR}/_ext/1472/main.o.d 
+	${MP_CC}  -omf=elf -x c -c -mcpu=24FJ128DA106 -Wall -I".." -I"../.." -I"../../blapi" -I"../../bootloader/microchip/include" -MMD -MF ${OBJECTDIR}/_ext/1472/main.o.d -o ${OBJECTDIR}/_ext/1472/main.o ../main.c 
+ifneq (,$(findstring MINGW32,$(OS_CURRENT))) 
+	 sed -e 's/\"//g' -e 's/\\$$/__EOL__/g' -e 's/\\ /__ESCAPED_SPACES__/g' -e 's/\\/\//g' -e 's/__ESCAPED_SPACES__/\\ /g' -e 's/__EOL__$$/\\/g' ${OBJECTDIR}/_ext/1472/main.o.d > ${OBJECTDIR}/_ext/1472/main.o.tmp
+	${RM} ${OBJECTDIR}/_ext/1472/main.o.d 
+	${CP} ${OBJECTDIR}/_ext/1472/main.o.tmp ${OBJECTDIR}/_ext/1472/main.o.d 
+	${RM} ${OBJECTDIR}/_ext/1472/main.o.tmp}
+else 
+	 sed -e 's/\"//g' ${OBJECTDIR}/_ext/1472/main.o.d > ${OBJECTDIR}/_ext/1472/main.o.tmp
+	${RM} ${OBJECTDIR}/_ext/1472/main.o.d 
+	${CP} ${OBJECTDIR}/_ext/1472/main.o.tmp ${OBJECTDIR}/_ext/1472/main.o.d 
+	${RM} ${OBJECTDIR}/_ext/1472/main.o.tmp}
+endif
+${OBJECTDIR}/_ext/1846862476/uart2.o: ../../bootloader/microchip/common/uart2.c  nbproject/Makefile-${CND_CONF}.mk
+	${MKDIR} ${OBJECTDIR}/_ext/1846862476 
+	${RM} ${OBJECTDIR}/_ext/1846862476/uart2.o.d 
+	${MP_CC}  -omf=elf -x c -c -mcpu=24FJ128DA106 -Wall -I".." -I"../.." -I"../../blapi" -I"../../bootloader/microchip/include" -MMD -MF ${OBJECTDIR}/_ext/1846862476/uart2.o.d -o ${OBJECTDIR}/_ext/1846862476/uart2.o ../../bootloader/microchip/common/uart2.c 
+ifneq (,$(findstring MINGW32,$(OS_CURRENT))) 
+	 sed -e 's/\"//g' -e 's/\\$$/__EOL__/g' -e 's/\\ /__ESCAPED_SPACES__/g' -e 's/\\/\//g' -e 's/__ESCAPED_SPACES__/\\ /g' -e 's/__EOL__$$/\\/g' ${OBJECTDIR}/_ext/1846862476/uart2.o.d > ${OBJECTDIR}/_ext/1846862476/uart2.o.tmp
+	${RM} ${OBJECTDIR}/_ext/1846862476/uart2.o.d 
+	${CP} ${OBJECTDIR}/_ext/1846862476/uart2.o.tmp ${OBJECTDIR}/_ext/1846862476/uart2.o.d 
+	${RM} ${OBJECTDIR}/_ext/1846862476/uart2.o.tmp}
+else 
+	 sed -e 's/\"//g' ${OBJECTDIR}/_ext/1846862476/uart2.o.d > ${OBJECTDIR}/_ext/1846862476/uart2.o.tmp
+	${RM} ${OBJECTDIR}/_ext/1846862476/uart2.o.d 
+	${CP} ${OBJECTDIR}/_ext/1846862476/uart2.o.tmp ${OBJECTDIR}/_ext/1846862476/uart2.o.d 
+	${RM} ${OBJECTDIR}/_ext/1846862476/uart2.o.tmp}
+endif
+endif
+
+# ------------------------------------------------------------------------------------
+# Rules for buildStep: link
+ifeq ($(TYPE_IMAGE), DEBUG_RUN)
+dist/${CND_CONF}/${IMAGE_TYPE}/IOIOApp.X.${IMAGE_TYPE}.elf: ${OBJECTFILES}  nbproject/Makefile-${CND_CONF}.mk
+	${MKDIR} dist/${CND_CONF}/${IMAGE_TYPE} 
+	${MP_CC}  -omf=elf  -mcpu=24FJ128DA106  -D__DEBUG -D__MPLAB_DEBUGGER_PK3=1 -o dist/${CND_CONF}/${IMAGE_TYPE}/IOIOApp.X.${IMAGE_TYPE}.elf ${OBJECTFILES}      -Wl,--defsym=__MPLAB_BUILD=1,--script=../../blapi/app_p24FJ128DA106.gld,--defsym=__MPLAB_DEBUG=1,--defsym=__ICD2RAM=1,--defsym=__DEBUG=1,--defsym=__MPLAB_DEBUGGER_PK3=1,-L"../../blapi",-Map="$(BINDIR_)$(TARGETBASE).map",--report-mem
+else
+dist/${CND_CONF}/${IMAGE_TYPE}/IOIOApp.X.${IMAGE_TYPE}.elf: ${OBJECTFILES}  nbproject/Makefile-${CND_CONF}.mk
+	${MKDIR} dist/${CND_CONF}/${IMAGE_TYPE} 
+	${MP_CC}  -omf=elf  -mcpu=24FJ128DA106  -o dist/${CND_CONF}/${IMAGE_TYPE}/IOIOApp.X.${IMAGE_TYPE}.elf ${OBJECTFILES}      -Wl,--defsym=__MPLAB_BUILD=1,--script=../../blapi/app_p24FJ128DA106.gld,-L"../../blapi",-Map="$(BINDIR_)$(TARGETBASE).map",--report-mem
+	${MP_CC_DIR}/pic30-bin2hex dist/${CND_CONF}/${IMAGE_TYPE}/IOIOApp.X.${IMAGE_TYPE}.elf -omf=elf
+endif
+
+
+# Subprojects
+.build-subprojects:
+
+# Clean Targets
+.clean-conf:
+	${RM} -r build/default
+	${RM} -r dist
+# Enable dependency checking
+.dep.inc: .depcheck-impl
+
+include .dep.inc
---
+++ firmware/app/IOIOApp.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=IOIOApp.X.production.elf
+CND_ARTIFACT_PATH_default=dist/default/production/IOIOApp.X.production.elf
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=ioioapp.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/ioioapp.x.tar
