bcafd0471d0daf5c9acbb505ec0280f7c6e8b2a0
Ytai Ben-Tsvi <ytaibt@gmail.com>
2011-02-05 21:49:22 +0200

    fix bug in buffer locking

--- firmware/app_layer_v1/byte_queue.h
+++ firmware/app_layer_v1/byte_queue.h
@@ -14,11 +14,10 @@ typedef struct {
 } ByteQueue;
 
 #define DEFINE_STATIC_BYTE_QUEUE(name, size)              \
-  static BYTE name##ipl_save;                             \
   static BYTE name##_buf[size];                           \
   static ByteQueue name = { name##_buf, size, 0, 0, 0 }
 
-#define ByteQueueLock(q) q##ipl_save = SRbits.IPL; SRbits.IPL = 1;
+#define ByteQueueLock(q) BYTE q##ipl_save = SRbits.IPL; SRbits.IPL = 1;
 #define ByteQueueUnlock(q) SRbits.IPL = q##ipl_save; 
 
 
