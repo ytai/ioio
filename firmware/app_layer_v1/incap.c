03c23c4b84e3b3efcc050c628d182024f5af3fa6
Ytai Ben-Tsvi <ytaibt@gmail.com>
2011-03-24 08:28:21 +0200

    a little progress on input capture. pausing this effort now.

--- firmware/app_layer_v1/incap.c
+++ firmware/app_layer_v1/incap.c
@@ -16,7 +16,7 @@ typedef struct {
   unsigned int tmr;
 } INCAP_REG;
 
-volatile INCAP_REG* incap_regs = (volatile INCAP_REG *) &IC1CON1;
+static volatile INCAP_REG* incap_regs = (volatile INCAP_REG *) &IC1CON1;
 
 void InCapInit() {
   log_printf("InCapInit()");
@@ -28,7 +28,14 @@ void InCapInit() {
 
 void InCapConfig(int incap_num, int mode, int continouos, int clock_scale,
                  int input_scale) {
+  volatile INCAP_REG* reg = incap_regs + incap_num;
   log_printf("InCapConfig(%d, %d, %d, %d, %d)", incap_num, mode, continouos,
              clock_scale, input_scale);
-  Set_ICIF[2](0);
+  Set_ICIE[incap_num](0);  // disable interrupts
+  reg->con1 = 0x0000;  // disable module
+  Set_ICIF[incap_num](0);  // clear interrupts
+  Set_ICIP[incap_num](1);  // interrupt priority 1
+  if (mode) {
+    
+  }
 }
