bd785ce4ee49feb1d8db58a6ad6c1e4c931b3077
Ytai Ben-Tsvi <ytaibt@gmail.com>
2011-06-03 22:24:46 +0300

    Make torture test use AbstractIOIOActivity in preparation for ADK.

--- software/applications/IOIOTortureTest/src/ioio/tests/torture/MainActivity.java
+++ software/applications/IOIOTortureTest/src/ioio/tests/torture/MainActivity.java
@@ -1,18 +1,12 @@
 package ioio.tests.torture;
 
-import ioio.lib.api.IOIO;
 import ioio.lib.api.IOIO.VersionType;
-import ioio.lib.api.IOIOFactory;
 import ioio.lib.api.exception.ConnectionLostException;
-import ioio.lib.api.exception.IncompatibilityException;
-import android.app.Activity;
+import ioio.lib.util.AbstractIOIOActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.TextView;
 
-public class MainActivity extends Activity {
-	private IOIOThread ioio_thread_;
-
+public class MainActivity extends AbstractIOIOActivity {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -20,74 +14,40 @@ public class MainActivity extends Activity {
 	}
 
 	@Override
-	protected void onResume() {
-		super.onResume();
-		ioio_thread_ = new IOIOThread();
-		ioio_thread_.start();
+	protected AbstractIOIOActivity.IOIOThread createIOIOThread() {
+		return new IOIOThread();
 	}
 
-	@Override
-	protected void onPause() {
-		super.onPause();
-		ioio_thread_.abort();
-		try {
-			ioio_thread_.join();
-		} catch (InterruptedException e) {
-		}
-	}
 
-	class IOIOThread extends Thread {
-		private IOIO ioio_;
-		private boolean abort_ = false;
+	class IOIOThread extends AbstractIOIOActivity.IOIOThread {
 		private ResourceAllocator alloc_ = new ResourceAllocator();
 		private TestProvider provider_;
 		private TestThread[] workers_ = new TestThread[8];
-
+		
 		@Override
-		public void run() {
-			super.run();
-			while (true) {
-				synchronized (this) {
-					if (abort_) {
-						break;
-					}
-					ioio_ = IOIOFactory.create();
-					provider_ = new TestProvider(MainActivity.this, ioio_,
-							alloc_);
-				}
-				try {
-					ioio_.waitForConnect();
-					showVersions();
-					//Debug.startMethodTracing();
-					for (int i = 0; i < workers_.length; ++i) {
-						workers_[i] = new TestThread(provider_);
-						workers_[i].start();
-					}
-				} catch (ConnectionLostException e) {
-				} catch (IncompatibilityException e) {
-					Log.e("TortureTest", "Incompatibility detected", e);
-					break;
-				} catch (Exception e) {
-					Log.e("TortureTest", "Unexpected exception caught", e);
-					ioio_.disconnect();
-					break;
-				} finally {
-					//Debug.stopMethodTracing();
+		protected void setup() throws ConnectionLostException {
+			provider_ = new TestProvider(MainActivity.this, ioio_,
+					alloc_);
+			showVersions();
+			//Debug.startMethodTracing();
+			for (int i = 0; i < workers_.length; ++i) {
+				workers_[i] = new TestThread(provider_);
+				workers_[i].start();
+			}
+			try {
+				ioio_.waitForDisconnect();
+			} catch (InterruptedException e) {
+			}
+			for (int i = 0; i < workers_.length; ++i) {
+				if (workers_[i] != null) {
+					workers_[i].interrupt();
 					try {
-						ioio_.waitForDisconnect();
-						Log.i("TortureTest",
-								"IOIO disconnected, killing workers");
-						for (int i = 0; i < workers_.length; ++i) {
-							if (workers_[i] != null) {
-								workers_[i].interrupt();
-								workers_[i].join();
-							}
-						}
-						Log.i("TortureTest", "All workers dead");
+						workers_[i].join();
 					} catch (InterruptedException e) {
 					}
 				}
 			}
+			throw new ConnectionLostException();
 		}
 
 		private void showVersions() throws ConnectionLostException {
@@ -107,14 +67,6 @@ public class MainActivity extends Activity {
 					((TextView) findViewById(R.id.versions)).setText(versionText);
 				}
 			});
-		}
-
-		synchronized public void abort() {
-			abort_ = true;
-			if (ioio_ != null) {
-				ioio_.disconnect();
-			}
-			interrupt();
 		}
 	}
 }
