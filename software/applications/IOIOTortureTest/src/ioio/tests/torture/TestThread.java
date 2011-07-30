491d9d681d0fb2e36e13a9c16a2060958218d96e
Ytai Ben-Tsvi <ytaibt@gmail.com>
2011-07-30 23:33:26 +0300

    better handling of test timeouts

--- software/applications/IOIOTortureTest/src/ioio/tests/torture/TestThread.java
+++ software/applications/IOIOTortureTest/src/ioio/tests/torture/TestThread.java
@@ -17,22 +17,21 @@ class TestThread extends Thread {
 	public void run() {
 		super.run();
 		while (true) {
+			Timer t = new Timer();
+			t.schedule(new TimerTask() {
+				@Override
+				public void run() {
+					StackTraceElement[] stackTrace = TestThread.this.getStackTrace();
+					Log.e("TortureTest", "====== Hung test stack trace ======");
+					for (StackTraceElement el : stackTrace) {
+						Log.e("TortureTest", el.toString());
+					}
+				}
+			}, 10000);
 			try {
 				TestRunner runner = provider_.newTest();
 				Log.i("TortureTest", "TestThread [" + getId() + "] starting test " + runner.testClassName());
-				Timer t = new Timer();
-				t.schedule(new TimerTask() {
-					@Override
-					public void run() {
-						StackTraceElement[] stackTrace = TestThread.this.getStackTrace();
-						Log.e("TortureTest", "====== Hung test stack trace ======");
-						for (StackTraceElement el : stackTrace) {
-							Log.e("TortureTest", el.toString());
-						}
-					}
-				}, 10000);
 				runner.run();
-				t.cancel();
 				Log.i("TortureTest", "TestThread [" + getId() + "] finished test " + runner.testClassName());
 			} catch (InterruptedException e) {
 				Log.i("TortureTest", "TestThread [" + getId() + "] interrupted, exiting.");
@@ -41,6 +40,8 @@ class TestThread extends Thread {
 				Log.i("TortureTest",
 						"IOIO connection lost, TestThread [" + getId() + "] exiting.");
 				break;
+			} finally {
+				t.cancel();
 			}
 		}
 	}
