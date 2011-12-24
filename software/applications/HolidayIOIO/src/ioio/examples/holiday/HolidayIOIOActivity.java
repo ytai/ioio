81e3f573185a924dd2e3788cd78e39ae60c20682
Ytai Ben-Tsvi <ytaibt@gmail.com>
2011-12-23 20:35:35 -0800

    Some bug fixes and improvements for holiday app.

--- software/applications/HolidayIOIO/src/ioio/examples/holiday/HolidayIOIOActivity.java
+++ software/applications/HolidayIOIO/src/ioio/examples/holiday/HolidayIOIOActivity.java
@@ -16,78 +16,94 @@ import android.util.Log;
 import android.widget.SeekBar;
 import android.widget.SeekBar.OnSeekBarChangeListener;
 
+/**
+ * A IOIO holiday application!
+ * Blink an LED strip by using random color from the Android camera.
+ * IOIO pin 3 -> clock
+ * IOIO pin 4 -> data
+ * Built for an LED strip such as this one:
+ * http://www.sparkfun.com/products/10312
+ * 
+ * That's just a sample application - use your best creativity to modify it!
+ *
+ * @author ytai
+ */
 public class HolidayIOIOActivity extends AbstractIOIOActivity {
 	private static final String TAG = "HolidayIOIO";
 	private Camera camera_;
-	byte[][] frameBuffers_ = new byte[2][];
 	RGB[] frame_ = null;
 	private int height_;
 	private int width_;
 	double frequency_;
-	double fadeSpeed_;
+	double fadeRate_;
+	private RGB tempRGB_ = new RGB();
+	byte[] buffer1_ = new byte[48];
+	byte[] buffer2_ = new byte[48];
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
-		((SeekBar) findViewById(R.id.frequencySeekBar)).setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
+		// The first seekbar controls the blink frequency.
+		final SeekBar freqSeekBar = (SeekBar) findViewById(R.id.frequencySeekBar);
+		updateFrequency(freqSeekBar, freqSeekBar.getProgress());
+		freqSeekBar.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
 			@Override
 			public void onStopTrackingTouch(SeekBar seekBar) {
 			}
-			
+
 			@Override
 			public void onStartTrackingTouch(SeekBar seekBar) {
 			}
-			
+
 			@Override
 			public void onProgressChanged(SeekBar seekBar, int progress,
 					boolean fromUser) {
-				frequency_ = Math.pow((double) progress / seekBar.getMax(), 2.0);
+				updateFrequency(seekBar, progress);
 			}
 		});
-		
-		((SeekBar) findViewById(R.id.fadeSeekBar)).setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
+
+		// The second seekbar controls the fade time.
+		final SeekBar fadeSeekBar = (SeekBar) findViewById(R.id.fadeSeekBar);
+		updateFade(fadeSeekBar, fadeSeekBar.getProgress());
+		fadeSeekBar.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
 			@Override
 			public void onStopTrackingTouch(SeekBar seekBar) {
 			}
-			
+
 			@Override
 			public void onStartTrackingTouch(SeekBar seekBar) {
 			}
-			
+
 			@Override
 			public void onProgressChanged(SeekBar seekBar, int progress,
 					boolean fromUser) {
-				fadeSpeed_ = Math.pow((double) progress / seekBar.getMax(), 2.0);
+				updateFade(seekBar, progress);
 			}
 		});
+
 	}
 
 	@Override
 	protected void onStart() {
+		// Start the camera oreview
 		camera_ = getCameraInstance();
 		Parameters params = camera_.getParameters();
 		params.setPreviewFormat(ImageFormat.NV21);
 		getSmallestPreviewSize(params);
 		params.setPreviewSize(width_, height_);
-		// params.setFlashMode(Parameters.FLASH_MODE_TORCH);
-		// params.setWhiteBalance(Parameters.WHITE_BALANCE_AUTO);
+		//params.setFlashMode(Parameters.FLASH_MODE_TORCH);
+		params.setWhiteBalance(Parameters.WHITE_BALANCE_AUTO);
 		camera_.setParameters(params);
-		final Size previewSize = camera_.getParameters().getPreviewSize();
-		final int bytePerPixel = 2;
-		final int bufferSize = previewSize.height * previewSize.width
-				* bytePerPixel;
-		frameBuffers_[0] = new byte[bufferSize];
-		frameBuffers_[1] = new byte[bufferSize];
 		frame_ = new RGB[width_ * height_];
 		for (int i = 0; i < frame_.length; ++i) {
-			frame_[i] = new RGB(0, 0, 0);
+			frame_[i] = new RGB();
 		}
 
 		camera_.setPreviewCallback(new PreviewCallback() {
 			@Override
 			public void onPreviewFrame(byte[] data, Camera camera) {
-				synchronized(frame_) {
+				synchronized (frame_) {
 					decodeYUV420SP(frame_, data, width_, height_);
 				}
 			}
@@ -96,6 +112,7 @@ public class HolidayIOIOActivity extends AbstractIOIOActivity {
 		super.onStart();
 	}
 
+	/** Chooses the smallest supported preview size. */
 	private void getSmallestPreviewSize(Parameters params) {
 		List<Size> supportedPreviewSizes = params.getSupportedPreviewSizes();
 		Size minSize = null;
@@ -111,20 +128,20 @@ public class HolidayIOIOActivity extends AbstractIOIOActivity {
 	@Override
 	protected void onStop() {
 		super.onStop();
+		// Stop the camera preview.
 		camera_.stopPreview();
 		camera_.setPreviewCallback(null);
 		camera_.release();
 	}
 
+	/** An RGB triplet. */
 	private static class RGB {
-		double r;
-		double g;
-		double b;
+		byte r;
+		byte g;
+		byte b;
 
-		RGB(double rr, double gg, double bb) {
-			r = rr;
-			g = gg;
-			b = bb;
+		RGB() {
+			clear();
 		}
 
 		public void clear() {
@@ -134,17 +151,18 @@ public class HolidayIOIOActivity extends AbstractIOIOActivity {
 
 	class IOIOThread extends AbstractIOIOActivity.IOIOThread {
 		private SpiMaster spi_;
-		private RGB tempRGB_ = new RGB(0, 0, 0);
-		byte[] buffer1_ = new byte[48];
-		byte[] buffer2_ = new byte[48];
 
 		@Override
 		protected void setup() throws ConnectionLostException {
-			spi_ = ioio_.openSpiMaster(5, 4, 3, 6, SpiMaster.Rate.RATE_250K);
+			spi_ = ioio_.openSpiMaster(5, 4, 3, 6, SpiMaster.Rate.RATE_50K);
 		}
 
 		@Override
 		protected void loop() throws ConnectionLostException {
+			// We have 32 LEDs. Each one of them gets lit with probability
+			// frequency_. If lit, we pick a random pixel from the current preview
+			// frame and use its color. Otherwise, we set the LED to black and
+			// setLed() will take care of gradual fading.
 			for (int i = 0; i < 32; i++) {
 				tempRGB_.clear();
 				if (Math.random() < frequency_) {
@@ -152,6 +170,11 @@ public class HolidayIOIOActivity extends AbstractIOIOActivity {
 				}
 				setLed(i, tempRGB_);
 			}
+			// Since SPI messages are limited to 64 bytes, and we need to send
+			// 96 bytes, we divide the message into two chunks of 48. We assume
+			// that the SPI clock is slow enough (50K) so that the second half
+			// will finish being sent to the IOIO before the first half
+			// finished transmission.
 			try {
 				spi_.writeReadAsync(0, buffer1_, buffer1_.length,
 						buffer1_.length, null, 0);
@@ -162,8 +185,8 @@ public class HolidayIOIOActivity extends AbstractIOIOActivity {
 			}
 		}
 
+		/** Choose a random pixel from the current preview frame. */
 		private void getRandomColor(RGB rgb) {
-			rgb.clear();
 			synchronized (frame_) {
 				final int i = (int) (Math.random() * frame_.length);
 				rgb.r = frame_[i].r;
@@ -172,7 +195,12 @@ public class HolidayIOIOActivity extends AbstractIOIOActivity {
 			}
 		}
 
+		/**
+		 * Set an LED to a certain color.
+		 * If black is applied, the LED will fade out.
+		 */
 		private void setLed(int num, RGB rgb) {
+			// Find the right buffer to write to (first or second half).
 			byte[] buffer;
 			if (num >= 16) {
 				buffer = buffer2_;
@@ -182,25 +210,27 @@ public class HolidayIOIOActivity extends AbstractIOIOActivity {
 			}
 			num *= 3;
 			if (rgb.r == 0 && rgb.g == 0 && rgb.b == 0) {
-				fade(buffer, num++);
-				fade(buffer, num++);
-				fade(buffer, num++);
+				fadeOut(buffer, num++);
+				fadeOut(buffer, num++);
+				fadeOut(buffer, num++);
 			} else {
-				setLedComponent(buffer, num++, rgb.r);
-				setLedComponent(buffer, num++, rgb.g);
-				setLedComponent(buffer, num++, rgb.b);
+				// Poor-man's white balanace :)
+				buffer[num++] = fixColor(rgb.r, 0.9);
+				buffer[num++] = rgb.g;
+				buffer[num++] = fixColor(rgb.b, 0.5);
 			}
 		}
 
-		private void setLedComponent(byte[] buffer, int num, double value) {
-			value = value * value;
-			buffer[num] = (byte) (value * 256);
+		/** Attenuates a brightness level. */
+		private byte fixColor(byte color, double attenuation) {
+			double d = (double) ((int) color & 0xFF) / 256;
+			d *= attenuation;
+			return (byte) (d * 256);
 		}
 
-		private void fade(byte[] buffer, int num) {
-			double value = ((double) buffer[num]) / 256;
-			value = Math.sqrt(value);
-			setLedComponent(buffer, num, value * fadeSpeed_);
+		private void fadeOut(byte[] buffer, int num) {
+			final int value = (int) buffer[num] & 0xFF;
+			buffer[num] = (byte) (value * fadeRate_);
 		}
 
 	}
@@ -208,6 +238,16 @@ public class HolidayIOIOActivity extends AbstractIOIOActivity {
 	@Override
 	protected AbstractIOIOActivity.IOIOThread createIOIOThread() {
 		return new IOIOThread();
+	}
+
+	/** Read the progress bar value and scale to [0-1]. */
+	private void updateFrequency(SeekBar seekBar, int progress) {
+		frequency_ = Math.pow((double) progress / seekBar.getMax(), 2.0);
+	}
+
+	/** Read the progress bar value and scale to [0-1]. */
+	private void updateFade(SeekBar seekBar, int progress) {
+		fadeRate_ = Math.pow((double) progress / seekBar.getMax(), 2.0);
 	}
 
 	private static Camera getCameraInstance() {
@@ -219,41 +259,72 @@ public class HolidayIOIOActivity extends AbstractIOIOActivity {
 		return null;
 	}
 
-	private static void decodeYUV420SP(RGB[] rgb, byte[] yuv420sp, int width, int height) {
-		final int frameSize = width * height;
-		for (int j = 0, yp = 0; j < height; j++) {
-			int uvp = frameSize + (j >> 1) * width, u = 0, v = 0;
-			for (int i = 0; i < width; i++, yp++) {
-				int y = (0xff & ((int) yuv420sp[yp])) - 16;
-				if (y < 0)
-					y = 0;
-				if ((i & 1) == 0) {
-					v = (0xff & yuv420sp[uvp++]) - 128;
-					u = (0xff & yuv420sp[uvp++]) - 128;
-				}
+	/**
+	 * Transform frame from YUV420 to RGB.
+	 * Based on code from http://code.google.com/p/ketai/
+	 * THANKS!
+	 */
+	private static void decodeYUV420SP(RGB[] rgb, byte[] yuvs, int width,
+			int height) {
+		final int lumEnd = width * height;
+		int lumPtr = 0;
+		int chrPtr = lumEnd;
+		int outPtr = 0;
+		int lineEnd = width;
 
-				int y1192 = 1192 * y;
-				int r = (y1192 + 1634 * v);
-				int g = (y1192 - 833 * v - 400 * u);
-				int b = (y1192 + 2066 * u);
-
-				if (r < 0)
-					r = 0;
-				else if (r > 262143)
-					r = 262143;
-				if (g < 0)
-					g = 0;
-				else if (g > 262143)
-					g = 262143;
-				if (b < 0)
-					b = 0;
-				else if (b > 262143)
-					b = 262143;
-
-				rgb[yp].r = ((double) r) / (1 << 18);
-				rgb[yp].g = ((double) g) / (1 << 18);
-				rgb[yp].b = ((double) b) / (1 << 18);
+		while (true) {
+			if (lumPtr == lineEnd) {
+				if (lumPtr == lumEnd)
+					break; // we've reached the end
+				chrPtr = lumEnd + ((lumPtr >> 1) / width) * width;
+				lineEnd += width;
 			}
+
+			final int Y1 = yuvs[lumPtr++] & 0xff;
+			final int Y2 = yuvs[lumPtr++] & 0xff;
+			final int Cr = (yuvs[chrPtr++] & 0xff) - 128;
+			final int Cb = (yuvs[chrPtr++] & 0xff) - 128;
+			int R, G, B;
+
+			// generate first RGB components
+			B = Y1 + ((454 * Cb) >> 8);
+			if (B < 0)
+				B = 0;
+			else if (B > 255)
+				B = 255;
+			G = Y1 - ((88 * Cb + 183 * Cr) >> 8);
+			if (G < 0)
+				G = 0;
+			else if (G > 255)
+				G = 255;
+			R = Y1 + ((359 * Cr) >> 8);
+			if (R < 0)
+				R = 0;
+			else if (R > 255)
+				R = 255;
+			rgb[outPtr].r = (byte) R;
+			rgb[outPtr].g = (byte) G;
+			rgb[outPtr++].b = (byte) B;
+
+			// generate second RGB components
+			B = Y2 + ((454 * Cb) >> 8);
+			if (B < 0)
+				B = 0;
+			else if (B > 255)
+				B = 255;
+			G = Y2 - ((88 * Cb + 183 * Cr) >> 8);
+			if (G < 0)
+				G = 0;
+			else if (G > 255)
+				G = 255;
+			R = Y2 + ((359 * Cr) >> 8);
+			if (R < 0)
+				R = 0;
+			else if (R > 255)
+				R = 255;
+			rgb[outPtr].r = (byte) R;
+			rgb[outPtr].g = (byte) G;
+			rgb[outPtr++].b = (byte) B;
 		}
 	}
 }
