445bbbcceff01ad763b46240d37b430a0ee6488b
Ytai Ben-Tsvi <ytaibt@gmail.com>
2012-01-18 23:16:10 -0800

    Minor cleanups

--- software/IOIOLibAccessory/src/ioio/lib/android/accessory/AccessoryConnectionBootstrap.java
+++ software/IOIOLibAccessory/src/ioio/lib/android/accessory/AccessoryConnectionBootstrap.java
@@ -45,7 +45,6 @@ import java.util.Collection;
 
 import android.app.Activity;
 import android.app.PendingIntent;
-import android.bluetooth.BluetoothAdapter;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
