0265b9310352641e7ec6f17f1354fd5c0459caf2
Ytai Ben-Tsvi <ytaibt@gmail.com>
2012-06-03 21:18:38 -0700

    added HelloIOIOMain example

---
+++ software/applications/pc/HelloIOIOMain/src/ioio/examples/hello_main/HelloIOIOMain.java
@@ -0,0 +1,70 @@
+package ioio.examples.hello_main;
+
+import ioio.lib.api.DigitalOutput;
+import ioio.lib.api.IOIO;
+import ioio.lib.api.exception.ConnectionLostException;
+import ioio.lib.util.BaseIOIOLooper;
+import ioio.lib.util.IOIOLooper;
+import ioio.lib.util.IOIOLooperProvider;
+import ioio.lib.util.pc.IOIOMain;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class HelloIOIOMain extends IOIOMain implements IOIOLooperProvider {
+	private boolean ledOn_ = false;
+
+	// Boilerplate main(). Copy-paste this code into any IOIOapplication.
+	public static void main(String[] args) throws Exception {
+		new HelloIOIOMain().go(args);
+	}
+
+	@Override
+	protected void run(String[] args) throws IOException {
+		BufferedReader reader = new BufferedReader(new InputStreamReader(
+				System.in));
+		boolean abort = false;
+		String line;
+		while (!abort && (line = reader.readLine()) != null) {
+			switch (line) {
+			case "t":
+				ledOn_ = !ledOn_;
+				break;
+			case "n":
+				ledOn_ = true;
+				break;
+			case "f":
+				ledOn_ = false;
+				break;
+			case "q":
+				abort = true;
+				break;
+			default:
+				System.out
+						.println("Unknown input. t=toggle, n=on, f=off, q=quit.");
+			}
+		}
+	}
+
+	@Override
+	public IOIOLooper createIOIOLooper(String connectionType, Object extra) {
+		return new BaseIOIOLooper() {
+			private DigitalOutput led_;
+
+			@Override
+			protected void setup() throws ConnectionLostException,
+					InterruptedException {
+				led_ = ioio_.openDigitalOutput(IOIO.LED_PIN, true);
+			}
+
+			@Override
+			public void loop() throws ConnectionLostException,
+					InterruptedException {
+				led_.write(!ledOn_);
+				Thread.sleep(10);
+			}
+		};
+	}
+
+}
