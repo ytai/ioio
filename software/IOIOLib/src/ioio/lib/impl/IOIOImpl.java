693b49e15feeeabcff1fc877fbfbe63247015a08
Ytai Ben-Tsvi <ytaibt@gmail.com>
2012-08-25 19:01:07 -0700

    increment version to 3.06/3.26

--- software/IOIOLib/src/ioio/lib/impl/IOIOImpl.java
+++ software/IOIOLib/src/ioio/lib/impl/IOIOImpl.java
@@ -311,7 +311,7 @@ public class IOIOImpl implements IOIO, DisconnectListener {
 		case APP_FIRMWARE_VER:
 			return incomingState_.firmwareId_;
 		case IOIOLIB_VER:
-			return "IOIO0324";
+			return "IOIO0326";
 		}
 		return null;
 	}
