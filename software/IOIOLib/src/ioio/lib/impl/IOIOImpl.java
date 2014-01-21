656e24eebaa483ed20aad9e59642625acdd00b3c
Ytai Ben-Tsvi <ytaibt@gmail.com>
2014-01-20 21:01:03 -0800

    IOIOLib: Increment version to IOIO0501

--- software/IOIOLib/src/ioio/lib/impl/IOIOImpl.java
+++ software/IOIOLib/src/ioio/lib/impl/IOIOImpl.java
@@ -262,7 +262,7 @@ public class IOIOImpl implements IOIO, DisconnectListener {
 		case APP_FIRMWARE_VER:
 			return incomingState_.firmwareId_;
 		case IOIOLIB_VER:
-			return "IOIO0500";
+			return "IOIO0501";
 		}
 		return null;
 	}
