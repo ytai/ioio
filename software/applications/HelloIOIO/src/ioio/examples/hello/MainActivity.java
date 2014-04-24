f8f8129cd1865b4819cdc72443a2d8cf7a87cdd3
Ytai Ben-Tsvi <ytaibt@gmail.com>
2014-04-23 23:07:43 -0700

    HelloIOIO: Improve UI
    - Show toasts with versions on connection, disconnection, incompatible firmware.
    - Enable UI only when a compatible IOIO is connected.

--- software/applications/HelloIOIO/src/ioio/examples/hello/MainActivity.java
+++ software/applications/HelloIOIO/src/ioio/examples/hello/MainActivity.java
@@ -1,16 +1,20 @@
 package ioio.examples.hello;
 
 import ioio.lib.api.DigitalOutput;
+import ioio.lib.api.IOIO;
+import ioio.lib.api.IOIO.VersionType;
 import ioio.lib.api.exception.ConnectionLostException;
 import ioio.lib.util.BaseIOIOLooper;
 import ioio.lib.util.IOIOLooper;
 import ioio.lib.util.android.IOIOActivity;
+import android.content.Context;
 import android.os.Bundle;
+import android.widget.Toast;
 import android.widget.ToggleButton;
 
 /**
  * This is the main activity of the HelloIOIO example application.
- * 
+ *
  * It displays a toggle button on the screen, which enables control of the
  * on-board LED. This example shows a very simple usage of the IOIO, by using
  * the {@link IOIOActivity} class. For a more advanced use case, see the
@@ -44,42 +48,107 @@ public class MainActivity extends IOIOActivity {
 		/**
 		 * Called every time a connection with IOIO has been established.
 		 * Typically used to open pins.
-		 * 
+		 *
 		 * @throws ConnectionLostException
 		 *             When IOIO connection is lost.
-		 * 
-		 * @see ioio.lib.util.AbstractIOIOActivity.IOIOThread#setup()
+		 *
+		 * @see ioio.lib.util.IOIOLooper#setup()
 		 */
 		@Override
 		protected void setup() throws ConnectionLostException {
+			showVersions(ioio_, "IOIO connected!");
 			led_ = ioio_.openDigitalOutput(0, true);
+			enableUi(true);
 		}
 
 		/**
 		 * Called repetitively while the IOIO is connected.
-		 * 
+		 *
 		 * @throws ConnectionLostException
 		 *             When IOIO connection is lost.
-		 * 
-		 * @see ioio.lib.util.AbstractIOIOActivity.IOIOThread#loop()
+		 * @throws InterruptedException
+		 * 				When the IOIO thread has been interrupted.
+		 *
+		 * @see ioio.lib.util.IOIOLooper#loop()
 		 */
 		@Override
-		public void loop() throws ConnectionLostException {
+		public void loop() throws ConnectionLostException, InterruptedException {
 			led_.write(!button_.isChecked());
-			try {
-				Thread.sleep(100);
-			} catch (InterruptedException e) {
-			}
+			Thread.sleep(100);
 		}
-	}
+
+		/**
+		 * Called when the IOIO is disconnected.
+		 *
+		 * @see ioio.lib.util.IOIOLooper#disconnected()
+		 */
+		@Override
+		public void disconnected() {
+			enableUi(false);
+			toast("IOIO disconnected");
+		}
+
+		/**
+		 * Called when the IOIO is connected, but has an incompatible firmware version.
+		 *
+		 * @see ioio.lib.util.IOIOLooper#incompatible(IOIO)
+		 */
+		@Override
+		public void incompatible() {
+			showVersions(ioio_, "Incompatible firmware version!");
+		}
+}
 
 	/**
 	 * A method to create our IOIO thread.
-	 * 
+	 *
 	 * @see ioio.lib.util.AbstractIOIOActivity#createIOIOThread()
 	 */
 	@Override
 	protected IOIOLooper createIOIOLooper() {
 		return new Looper();
+	}
+
+	private void showVersions(IOIO ioio, String title) {
+		toast(String.format("%s\n" +
+				"IOIOLib: %s\n" +
+				"Application firmware: %s\n" +
+				"Bootloader firmware: %s\n" +
+				"Hardware: %s",
+				title,
+				ioio.getImplVersion(VersionType.IOIOLIB_VER),
+				ioio.getImplVersion(VersionType.APP_FIRMWARE_VER),
+				ioio.getImplVersion(VersionType.BOOTLOADER_VER),
+				ioio.getImplVersion(VersionType.HARDWARE_VER)));
+	}
+
+	private void toast(final String message) {
+		final Context context = this;
+		runOnUiThread(new Runnable() {
+			@Override
+			public void run() {
+				Toast.makeText(context, message, Toast.LENGTH_LONG).show();
+			}
+		});
+	}
+
+	private int numConnected_ = 0;
+
+	private void enableUi(final boolean enable) {
+		// This is slightly trickier than expected to support a multi-IOIO use-case.
+		runOnUiThread(new Runnable() {
+			@Override
+			public void run() {
+				if (enable) {
+					if (numConnected_++ == 0) {
+						button_.setEnabled(true);
+					}
+				} else {
+					if (--numConnected_ == 0) {
+						button_.setEnabled(false);
+					}
+				}
+			}
+		});
 	}
 }
