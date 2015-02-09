463cedf01f0fb366477f6972d1ac685dd5e36472
ThanosFisherman <psaridis@gmail.com>
2015-02-09 04:00:24 +0200

    finally Log class

--- software/IOIOLibCore/src/ioio/lib/spi/Log.java
+++ software/IOIOLibCore/src/ioio/lib/spi/Log.java
@@ -41,106 +41,68 @@ public class Log {
 		/** Logs a debug message to the console or logcat */
 		public void v(String tag, String message, Throwable exception);
 
-		/**
-		 * Sets the log level. {@link #LOG_NONE} will mute all log output.
-		 * 
-		 * @param logLevel
-		 *            {@link #LOG_NONE}.
-		 */
-		public void setLogLevel(int logLevel);
-
-		/** Gets the log level. */
-		public int getLogLevel();
-
 	}
 	
 	private static ILogger log_;
 	
 	public static void i(String tag, String message) {
-		if (log_.getLogLevel() >= Log.LOG_INFO) {
 			log_.i(tag, message);
-		}
 	}
 
 	
 	public static void i(String tag, String message, Throwable exception) {
-		if (log_.getLogLevel() >= Log.LOG_INFO) {
 			log_.i(tag, message, exception);
-		}
 	}
 
 
 	public static void e(String tag, String message) {
-		if (log_.getLogLevel() >= Log.LOG_ERROR) {
 			log_.e(tag, message);
-		}
 	}
 
 	public static void e(String tag, String message, Throwable exception) {
-		if (log_.getLogLevel() >= Log.LOG_ERROR) {
 			log_.e(tag, message, exception);
-		}
 	}
 
 	
 	public static void d(String tag, String message) {
-		if (log_.getLogLevel() >= Log.LOG_DEBUG) {
 			log_.d(tag, message);
-		}
-
 	}
 
 	
 	public static void d(String tag, String message, Throwable exception) {
-		if (log_.getLogLevel() >= Log.LOG_DEBUG) {
 			log_.d(tag, message, exception);
-		}
 	}
 
 	
 	public static void w(String tag, String message) {
-		if (log_.getLogLevel()>= Log.LOG_WARN) {
 			log_.w(tag, message);
-		}
 	}
 
 	
 	public static void w(String tag, String message, Throwable exception) {
-		if (log_.getLogLevel() >= Log.LOG_WARN) {
 			log_.w(tag, message, exception);
-		}
 	}
 
 	public static void v(String tag, String message) {
-		if (log_.getLogLevel() >= Log.LOG_VERBOSE) {
 			log_.v(tag, message);
-		}
 	}
 
 	public static void v(String tag, String message, Throwable exception) {
-		if (log_.getLogLevel() >= Log.LOG_VERBOSE) {
 			log_.v(tag, message, exception);
-		}
-	}
-	
-	public static void setLogLevel(int logLevel) {
-		log_.setLogLevel(logLevel);
 	}
 
-	public static int getLogLevel() {
-		return log_.getLogLevel();
-	}
-
-	
-	public static void addLogBootstrap(String classPath)
+	static 
 	{
 		try {
-			Log.log_ = (ILogger) Class.forName(classPath).newInstance();
+			Log.log_ = (ILogger) Class.forName("ioio.lib.spi.LogImpl").newInstance();
 		} catch (InstantiationException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (IllegalAccessException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
