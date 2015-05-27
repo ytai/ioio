646ff2fa790ec6ad718b58ba9a44da994262b282
Ytai Ben-Tsvi <ytaibt@gmail.com>
2015-05-27 12:52:23 -0700

    IOIOManager: Fix linter warning.

--- software/applications/IOIOManager/src/main/java/ioio/manager/BootImageLibraryActivity.java
+++ software/applications/IOIOManager/src/main/java/ioio/manager/BootImageLibraryActivity.java
@@ -1,17 +1,17 @@
 /*
  * Copyright 2011 Ytai Ben-Tsvi. All rights reserved.
- *  
- * 
+ *
+ *
  * Redistribution and use in source and binary forms, with or without modification, are
  * permitted provided that the following conditions are met:
- * 
+ *
  *    1. Redistributions of source code must retain the above copyright notice, this list of
  *       conditions and the following disclaimer.
- * 
+ *
  *    2. Redistributions in binary form must reproduce the above copyright notice, this list
  *       of conditions and the following disclaimer in the documentation and/or other materials
  *       provided with the distribution.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED "AS IS" AND ANY EXPRESS OR IMPLIED
  * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
  * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL ARSHAN POURSOHI OR
@@ -21,7 +21,7 @@
  * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
  * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
  * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- * 
+ *
  * The views and conclusions contained in the software and documentation are those of the
  * authors and should not be interpreted as representing official policies, either expressed
  * or implied.
@@ -59,7 +59,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class BootImageLibraryActivity extends ExpandableListActivity {
-	private static final String TAG = "BootImageLibraryActivity";
+	private static final String TAG = "BootImageLibrary";
 	public static final String ACTION_SELECT = "SELECT";
 	public static final String ACTION_EDIT = "EDIT";
 	public static final String EXTRA_BUNDLE_NAME = "BUNDLE_NAME";
